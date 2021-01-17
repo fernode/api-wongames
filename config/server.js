78e0b0ba79799ad463e061c41f38f5b73c279b5a
Fernando Silva <fernandosilva@hotmail.com>
2021-01-16 21:58:12 -0300

    api structure data and graphQl

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'df8fa93fe883a9a924094a1ff307c5fa'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '575de308-557a-4e90-bcbb-318628471d2d'
+};
