07383c013cfca542b8d9bcbef8b0449fc01d3e7c
Fernando Silva <fernandosilva@hotmail.com>
2021-03-14 22:02:36 -0300

    feat - Fazendo scrapper dos dados de uma página com JSDOM

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -7,10 +7,26 @@
 
 const axios =  require("axios");
 
+async function gameInfo(slug) {
+  const jsdom = require("jsdom");
+  const {JSDOM} = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const description =  dom.window.document.querySelector(".description");
+
+  return {
+    rating: "BR0",
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML
+  }
+}
+
 module.exports = {
   populate: async (params)=>{
     const gogApi = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
     const {data: {products}} = await axios.get(gogApi);
-    console.log(products);
+
+    console.log(await gameInfo(products[0].slug));
   }
 };
