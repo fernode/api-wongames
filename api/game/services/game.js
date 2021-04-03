c9d3331828e82ccd9b78de9d86dbea08e5c58b13
Fernando Silva <fernandosilva@hotmail.com>
2021-04-03 19:16:40 -0300

    feat - add method createGames

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -8,7 +8,7 @@
 const axios = require("axios");
 const slugify = require("slugify");
 
-async function gameInfo(slug) {
+async function getGameInfo(slug) {
   const jsdom = require("jsdom");
   const { JSDOM } = jsdom;
   const body = await axios.get(`https://www.gog.com/game/${slug}`);
@@ -68,6 +68,40 @@ async function createManyToManyData(products) {
   ]);
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
 module.exports = {
   populate: async () => {
     const gogApi = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -76,6 +110,7 @@ module.exports = {
     } = await axios.get(gogApi);
 
     await createManyToManyData([products[2], products[3]]);
+    await createGames([products[2], products[3]]);
 
     // await create(products[1].publisher, "publisher");
     // await create(products[1].developer, "developer");
